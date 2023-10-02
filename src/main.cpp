15d1523135bdba7434d71ed2e9442927fee91ef3
Evgeniy S <sergeev.ev13@yandex.ru>
2023-10-02 23:26:13 +0300

    Generate config.h with version information

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "config.h"
 
 int main(int argc, char* argv[], char* env[])
 {
-    std::cout << "C++ CMake Template" << std::endl;
+    std::cout << "C++ CMake Template"
+        << " v" << TEMPLATE_PROJECT_VERSION_MAJOR << "." << TEMPLATE_PROJECT_VERSION_MINOR << std::endl;
 
     return 0;
 }
