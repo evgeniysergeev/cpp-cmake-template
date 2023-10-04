2770ffec6e62e94578dd4b286cb151a29f31168c
Evgeniy S <sergeev.ev13@yandex.ru>
2023-10-04 23:48:07 +0300

    Update version.h generation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <exception>
-#include "config.h"
+#include "version.h"
 
 static std::terminate_handler s_prev_termination_handler = nullptr;
 
@@ -20,12 +20,8 @@ int main(int argc, char* argv[], char* env[])
 {
     s_prev_termination_handler = std::set_terminate(termination_handler);
 
-    std::cout << "C++ CMake Template"
-        << " v" << TEMPLATE_PROJECT_VERSION << std::endl
-        << " (" << TEMPLATE_PROJECT_VERSION_MAJOR
-                << "." << TEMPLATE_PROJECT_VERSION_MINOR
-                << "." << TEMPLATE_PROJECT_VERSION_PATCH << "), "
-        << "(" << TEMPLATE_PROJECT_VERSION_TWEAK << ")"
+    std::cout << PROJECT_NAME
+        << " v" << PROJECT_VERSION 
         << std::endl;
 
     return 0;
