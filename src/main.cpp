dfd803d73605c1b5fbfb6674a24cfaa0db3fe903
Evgeniy S. <evgeniy.sergeev@gmail.com>
2023-10-04 12:43:07 +0300

    Add full version and other version details

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,12 @@
 int main(int argc, char* argv[], char* env[])
 {
     std::cout << "C++ CMake Template"
-        << " v" << TEMPLATE_PROJECT_VERSION_MAJOR << "." << TEMPLATE_PROJECT_VERSION_MINOR << std::endl;
+        << " v" << TEMPLATE_PROJECT_VERSION << std::endl
+        << " (" << TEMPLATE_PROJECT_VERSION_MAJOR
+                << "." << TEMPLATE_PROJECT_VERSION_MINOR
+                << "." << TEMPLATE_PROJECT_VERSION_PATCH << "), "
+        << "(" << TEMPLATE_PROJECT_VERSION_TWEAK << ")"
+        << std::endl;
 
     return 0;
 }
