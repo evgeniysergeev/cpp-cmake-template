147db699968e6e63a3a89e0d266cb5fc6bb1c9dc
Evgeniy S. <evgeniy.sergeev@gmail.com>
2023-10-04 13:53:13 +0300

    Install simple termination_handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <cstdlib>
+#include <exception>
 #include "config.h"
+
+static terminate_function s_prev_termination_handler = nullptr;
+
+static void termination_handler()
+{
+    std::cerr << "Unhandled exception" << std::endl << std::flush;
+
+    if (s_prev_termination_handler != nullptr) {
+      s_prev_termination_handler();
+    }
+
+    std::abort();
+}
 
 int main(int argc, char* argv[], char* env[])
 {
+    s_prev_termination_handler = std::set_terminate(termination_handler);
+
     std::cout << "C++ CMake Template"
         << " v" << TEMPLATE_PROJECT_VERSION << std::endl
         << " (" << TEMPLATE_PROJECT_VERSION_MAJOR
