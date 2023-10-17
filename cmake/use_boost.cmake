235bc4b4fed63127e7c7b1ad87e84f5c578d56d9
Evgeniy S. <sergeev.ev13@yandex.ru>
2023-10-17 18:14:03 +0300

    Add example option to use or not Boost Library

--- cmake/use_boost.cmake
+++ cmake/use_boost.cmake
@@ -1,44 +1,50 @@
 # add boost library
 
-# build boost using this command:
-# cd C:\Boost\boost_1_83_0
-# cd tools\build
-# bootstrap.bat
-# b2 --prefix=C:\Boost\boost_1_83_0\b2
-# add environment variable BOOST_ROOT=C:\Boost
-# and add to PATH C:\Boost\boost_1_83_0\b2
-# check that cmake is not from mingw installation using command: where cmake
-# check the same for b2: where b2
-# cd C:\Boost\boost_1_83_0
-# b2 -j8 variant=debug,release link=shared,static threading=single,multi runtime-link=shared,static address-model=32,64
-# now go to project directory to scripts\windows and check using generate.bat and build.bat scripts
+if(USE_BOOST)
 
-option(DEBUG_BOOST_CMAKE "Debug how Boost is added to CMake" OFF)
+  # build boost using this command:
+  # cd C:\Boost\boost_1_83_0
+  # cd tools\build
+  # bootstrap.bat
+  # b2 --prefix=C:\Boost\boost_1_83_0\b2
+  # add environment variable BOOST_ROOT=C:\Boost
+  # and add to PATH C:\Boost\boost_1_83_0\b2
+  # check that cmake is not from mingw installation using command: where cmake
+  # check the same for b2: where b2
+  # cd C:\Boost\boost_1_83_0
+  # b2 -j8 variant=debug,release link=shared,static threading=single,multi runtime-link=shared,static address-model=32,64
+  # now go to project directory to scripts\windows and check using generate.bat and build.bat scripts
 
-# This possibly can be useful if MinGW installed on Windows machine, but you want to use another Boost
-#set(Boost_NO_BOOST_CMAKE ON)
+  option(DEBUG_BOOST_CMAKE "Debug how Boost is added to CMake" OFF)
 
-set(Boost_USE_MULTITHREADED  ON)
-set(Boost_USE_STATIC_LIBS    ON)
-set(Boost_USE_STATIC_RUNTIME OFF)
+  # This possibly can be useful if MinGW installed on Windows machine, but you want to use another Boost
+  #set(Boost_NO_BOOST_CMAKE ON)
 
-if (DEBUG_BOOST_CMAKE)
-  set(Boost_DEBUG ON)
-else()
-  set(Boost_VERBOSE ON)
-endif()
+  set(Boost_USE_MULTITHREADED  ON)
+  set(Boost_USE_STATIC_LIBS    ON)
+  set(Boost_USE_STATIC_RUNTIME OFF)
 
-find_package(Boost COMPONENTS program_options REQUIRED)
-
-if(Boost_FOUND)
   if (DEBUG_BOOST_CMAKE)
-    message("Boost_LIBRARY_DIRS is set to: ${Boost_LIBRARY_DIRS}")
-    message("Boost_INCLUDE_DIRS is set to: ${Boost_INCLUDE_DIRS}")
+    set(Boost_DEBUG ON)
+  else()
+    set(Boost_VERBOSE ON)
   endif()
 
-  target_include_directories(Cpp-CMake-Template PUBLIC ${Boost_INCLUDE_DIRS})
-  link_directories(${Boost_LIBRARY_DIRS})
-  add_definitions(${Boost_DEFINITIONS})
-  target_link_libraries(Cpp-CMake-Template PUBLIC Boost::program_options)
+  find_package(Boost COMPONENTS program_options REQUIRED)
+
+  if(Boost_FOUND)
+    if (DEBUG_BOOST_CMAKE)
+      message("Boost_LIBRARY_DIRS is set to: ${Boost_LIBRARY_DIRS}")
+      message("Boost_INCLUDE_DIRS is set to: ${Boost_INCLUDE_DIRS}")
+    endif()
+
+    target_include_directories(Cpp-CMake-Template PUBLIC ${Boost_INCLUDE_DIRS})
+    link_directories(${Boost_LIBRARY_DIRS})
+    add_definitions(${Boost_DEFINITIONS})
+    target_link_libraries(Cpp-CMake-Template PUBLIC Boost::program_options)
+
+    target_compile_definitions(Cpp-CMake-Template PRIVATE "USE_BOOST")
+
+  endif()
 
 endif()
