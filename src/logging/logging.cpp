6d150ddb59201b840a226325bc30f37642a18e07
Evgeniy S. <sergeev.ev13@yandex.ru>
2023-11-17 21:57:05 +0300

    Minor fixup in logging to file

--- src/logging/logging.cpp
+++ src/logging/logging.cpp
@@ -9,14 +9,16 @@ namespace keywords = boost::log::keywords;
 
 void InitLogging(const std::string &log_file_name)
 {
+  logging::add_common_attributes();
+
   logging::add_file_log(log_file_name);
 
   logging::add_file_log
   (
       keywords::file_name = log_file_name,
       keywords::rotation_size = 10 * 1024 * 1024,
-      keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0)//,
-      //keywords::format = "[%TimeStamp%]: %Message%"
+      keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0),
+      keywords::format = "[%TimeStamp%]: %Message%"
   );
 
   logging::core::get()->set_filter
